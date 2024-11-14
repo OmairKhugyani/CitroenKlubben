152f4c5c70f11f3129d4944dd21e8057196faf4e
OmairKhugyani <115149701+OmairKhugyani@users.noreply.github.com>
2024-11-14 05:20:29 +0100

    db/CitroenKlubben.db

--- config.php
+++ config.php
@@ -2,5 +2,8 @@
 // Opret forbindelse til SQLite-databasen
 $db = new PDO('sqlite:db/CitroenKlubben.db');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
+
+// Opret forbindelse til SQLite-databasen
+$db = new PDO('sqlite:/Users/omairkhugyani/Desktop/DATAMATIKER/5.Semester/Hovedopgave/CitroenKlubben/db/CitroenKlubben.db');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
