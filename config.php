c60a1f5a357aaba36db74e83e855bedaae0ef5b2
philipv99 <philipv9913@gmail.com>
2024-11-14 14:28:44 +0100

    ret data base

--- config.php
+++ config.php
@@ -3,7 +3,4 @@
 $db = new PDO('sqlite:db/CitroenKlubben.db');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// Opret forbindelse til SQLite-databasen
-$db = new PDO('sqlite:/Users/omairkhugyani/Desktop/DATAMATIKER/5.Semester/Hovedopgave/CitroenKlubben/db/CitroenKlubben.db');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+?>
