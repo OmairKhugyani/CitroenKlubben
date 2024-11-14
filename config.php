9b161a974776c1a97493ae137ad0a4763152bdc8
OmairKhugyani <115149701+OmairKhugyani@users.noreply.github.com>
2024-11-14 03:17:14 +0100

    oprettelse af mapper og filer

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Opret forbindelse til SQLite-databasen
-$db = new PDO('sqlite:db/citroenklubben.db');
+$db = new PDO('sqlite:db/CitroenKlubben.db');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
 
